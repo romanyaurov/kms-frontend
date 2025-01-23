731bf1ab678f078dd514b862d44afdd8aa49061c
Roman Yaurov <yaurov.r@gmail.com>
2025-01-23 22:22:51 +0300

    edit env for test

--- apps/kms-desktop/src/environments/environment.ts
+++ apps/kms-desktop/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api_url: 'http://localhost:3000/api',
-  static_url: 'http://localhost:3000/static',
+  api_url: '/api',
+  static_url: '/static',
 };
