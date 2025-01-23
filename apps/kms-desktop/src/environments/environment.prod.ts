fdd56e5d7bd9991d1ac1dd015794274ffb3efcb1
Roman Yaurov <yaurov.r@gmail.com>
2025-01-23 18:41:55 +0300

    fix: change build options & add build folder

--- apps/kms-desktop/src/environments/environment.prod.ts
+++ apps/kms-desktop/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://85.193.88.175/api',
-  static_url: 'http://85.193.88.175/static',
+  api_url: 'http://localhost:3000/api',
+  static_url: 'http://localhost:3000/static',
 };
